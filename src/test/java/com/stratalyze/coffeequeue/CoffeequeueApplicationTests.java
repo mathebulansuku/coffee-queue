36c6af8897d2aca8ff188a93299f44a0d2c0d14e
mathebulansuku <mathebulansuku@gmail.com>
2025-11-21 22:25:31 +0000

    fixed unit test

--- src/test/java/com/stratalyze/coffeequeue/CoffeequeueApplicationTests.java
+++ src/test/java/com/stratalyze/coffeequeue/CoffeequeueApplicationTests.java
@@ -1,4 +1,4 @@
-package com.strartalyze.coffeequeue;
+package com.stratalyze.coffeequeue;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class CoffeequeueApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
